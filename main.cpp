8fff982c106121f4458a011755e00c7f74c40594
logued <logued@dkit.ie>
2026-02-08 09:33:57 +0000

    minor comment updates for clarity

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
+#include <iostream>         // Feb 2026
 using namespace std;
 // Demonstrates:
 // Use of sizeof() to calculate the number of elements in an array.
 // In main(), sizeof(array) will return 20 bytes, because there are 5 elements
 // each 4 bytes long (int length).  The length can be obtained because the runtime
-// system know that "array" is an array of integers,
+// system understands that this "array" is an array of integers,
 // and the length of the array is available to it.
 
-// Using sizeof(arr), in the function display() is different.
-// In this case, the parameter "arr" is really of type pointer to int (int*),
-// so the size of a pointer to an int is returned - 8 bytes.
-// There is no information available from the pointer about
+// Using sizeof(arr), in the function display() is interpreted differently.
+// In this case, the parameter "arr" is really of type 'pointer to int' (int*),
+// so, the size of a 'pointer to an int' is returned - 8 bytes.
+// There is no information available solely from the pointer about
 // the length of the array.  Therefore, we must pass in the length
-// of the array.  Although "array" and "arr" look the same, they are
-// actually different.
+// of the array to the function.  Although "array" and "arr" look the same,
+// they have different interpretations.
 
 
 void display(int arr[], int length) {
